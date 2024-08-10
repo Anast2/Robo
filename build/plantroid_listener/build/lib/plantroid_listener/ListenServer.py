5b8685bb8d17332300a91c8a8ae57496c9945858
Antonio Gonzalez <angacego@gmail.com>
2024-08-10 20:50:00 +0000

    Social plantroid

---
+++ build/plantroid_listener/build/lib/plantroid_listener/ListenServer.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+import speech_recognition as sr
+import uuid
+from time import time
+import subprocess
+import sys 
+sys.path.append('/home/plantroid/plantroid_ws/src/plantroid_listener/plantroid_listener')
+import VAD as vad
+
+def save_audio(path, data):
+    import wave
+    import pyaudio
+    from struct import pack
+    with open(path, "wb") as file:
+        file.write(data.get_wav_data())
+        file.close()
+
+
+class ListenServer(Node):
+
+    block = False
+    language = "en-US"
+    def __init__(self):
+        super().__init__('listen_server')
+        self.block_time = time()
+        self.publisher = self.create_publisher(String, 'messageTopic', 10)
+
+    def block_callback(self, message):
+        self.block = not self.block
+        if self.block: self.block_time = time()
+        self.get_logger().info(
+        "Changed Listening status to: " + str(not self.block))
+
+    def listenCallback(self, recognizer, audio):
+        if self.block:
+            if time()-self.block_time>15:
+                self.block = not self.block
+        else:
+            print("callback called")
+            try:
+                #phrase = recognizer.recognize_sphinx(audio,
+                #                                      language=self.language)
+                phrase = recognizer.recognize_google(audio,
+                                                     language=self.language)
+                filename = str(uuid.uuid4())
+                print("HEARD: " + phrase)
+                save_audio("./mem/audio/"+filename, audio)
+                self.Publish(phrase+";"+filename)
+            except sr.UnknownValueError:
+                print("PockectShphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from PockectShphinx\
+                    Recognition service; {0}".format(e))
+
+    def start_listening(self, recognizer):
+        print("Starting Listening subprocess...")
+        return recognizer.listen_in_background(m, self.listenCallback)
+
+    def stop_listening(self, listener):
+        print("Stopping Listening subprocess...")
+        try:
+            listener(wait_for_stop=False)
+            print("Successfully started Listening subprocess.")
+            return True
+        except:
+            print("ERROR: Could not stop the listener subprocess.")
+            return False
+
+    def Publish(self, message):
+        print("Trying to publish: ", message)
+        if not isinstance(message,str):
+            message = str(message)
+        try:
+            msg = String()
+            msg.data = message
+            self.publisher.publish(msg)
+            self.get_logger().info("Published: " + message)
+            return True
+        except Exception as e:
+            print(e)
+            print("ERROR: could not send the message through the publisher node.")
+            return False
+
+    def getBlock(self):
+        self.subscription = self.create_subscription(String,
+                                                     'ListenBlockTopic',
+                                                     self.block_callback,
+                                                     10)
+        self.subscription
+
+def main():
+    rclpy.init(args=None)
+    m = sr.Microphone()
+    r = sr.Recognizer()
+    vad.noise_calibration(m,r)
+
+    listen_server = ListenServer()
+    listen_server.start_listening(r)
+    listen_server.getBlock()
+    rclpy.spin(listen_server)
+
+if __name__ == "__main__":
+    main()
---
+++ src/movement_module/movement_module/NeuralNav.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import os
+import tflite_runtime.interpreter as tflite
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+#file is too big for github, you need to give your own path to the model
+#model_location = "/home/plantroid/vgg16_vis_nav_2.h5" 
+#modelH5_location = "/home/plantroid/dev_ws/src/plantroid/plantroid/stitched10_model_meeting_room.h5"#stitched_model_all_environments.h5" #"/home/plantroid/vgg16_vis_nav_2.h5" 
+modelH5_location = "/home/plantroid/dev_ws/src/plantroid/plantroid/stitched_model_meeting_prunning.h5"
+
+#model_location = "/home/plantroid/dev_ws/src/plantroid/plantroid/stitched_model16.tflite" 
+#model_location = "/home/plantroid/dev_ws/src/plantroid/plantroid/stitched_model10.tflite"
+model_location = "/home/plantroid/dev_ws/src/plantroid/plantroid/pruned.tflite"
+ 
+mynetH5 = tf.keras.models.load_model(modelH5_location, compile=False)
+
+def NeuralNavigationH5(vision, theta, theta_line, distance):
+    model = mynetH5
+    return model([np.array([vision/255]), np.array([[(theta_line+np.pi)/(2*np.pi), (theta+np.pi)/(2*np.pi), distance/23.1]])])[0][0]*2*np.pi-np.pi
+
+mynet = tflite.Interpreter(model_path=model_location, num_threads=1)
+
+#gpu_options = tf.lite.experimental.GpuDelegateOptionsV2()
+#gpu_delegate = tf.lite.experimental.GpuDelegateV2(options=gpu_options)
+#mynet.modify_graph_with_delegate(gpu_delegate)
+
+mynet.allocate_tensors()
+input_details = mynet.get_input_details()
+output_details = mynet.get_output_details()
+
+
+def NeuralNavigation(vision, theta, theta_line, distance):
+
+    vision_input = np.array([vision / 255], dtype=np.float32)
+    vector_input = np.array([[(theta_line + np.pi) / (2 * np.pi), (theta + np.pi) / (2 * np.pi), distance / 23.1]], dtype=np.float32)
+
+    mynet.set_tensor(input_details[0]['index'], vision_input)
+    mynet.set_tensor(input_details[1]['index'], vector_input)
+    mynet.invoke()
+
+    output_data = mynet.get_tensor(output_details[0]['index'])
+    
+    return output_data[0][0] * 2 * np.pi - np.pi
+
+    
+#while 1: print("I'm working")
