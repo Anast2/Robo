673e613cc06d5be11f79755cf75210d63e8190d0
AntonioGalizaCG <antonio.galiza@protonmail.com>
2024-11-19 17:28:11 +0100

    Added support to Balacoon TTS

--- src/rooted_speech_synthesizer/rooted_speech_synthesizer/speech_synthesis_interfaces.py
+++ src/rooted_speech_synthesizer/rooted_speech_synthesizer/speech_synthesis_interfaces.py
@@ -20,8 +20,25 @@ def tortoise_request(msg): pass  # Heavy model, but high quality results. Calls 
 #  TODO: implement
 
 
-def balacoon(msg): pass  # lighter model good for edge devices, lower quality results.
-#  TODO: implement
+def balacoon(msg): # lighter model good for edge devices, lower quality results.
+    data = msg.data
+    speech = ""
+    try:
+        data = literal_eval(data)
+        speech = data[0]
+    except Exception as e:
+        print(e)
+        speech = data
+    
+    tts = TTS("/home/antoniogaliza/ResearchWork/rooted_sppech_synthesizer")
+    supported_speakers = tts.get_speakers()
+    speaker = supported_speakers[-1]
+    samples = tts.synthesize(speech, speaker)
+    with wave.open("speech.wav", "w") as fp:
+        fp.setparams((1, 2, tts.get_sampling_rate(), len(samples), "NONE", "NONE"))
+        fp.writeframes(samples)    
+    cmd = ["play", "speech.wav"]
+    return subprocess.Popen(cmd)
 
 
 def balacoon_request(msg): pass  # lighter model good for edge devices, lower quality results. Calls remote server, but adds latency.
@@ -44,7 +61,11 @@ def tts_call(msg, mode="local", model="espeak_ng", IP=None, PORT=None):  #
     
     elif model == "balacoon":
         if mode == "local": 
-            balacoon(msg)
+            process = balacoon(msg)
+            process_done = process.poll() is None
+            while process_done:
+                process_done = process.poll() is None
+            print("done")
         else:
             balacoon_request(msg, IP, PORT)
     
